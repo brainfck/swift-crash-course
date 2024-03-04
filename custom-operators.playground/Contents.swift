0958c04fb6c6f82883e2acf4f90a7445b246919c
brainfck <mykhailo.tsuber@gmail.com>
2024-03-04 19:24:56 +0100

    modification

--- custom-operators.playground/Contents.swift
+++ custom-operators.playground/Contents.swift
@@ -1,3 +1,96 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let firstName: String? = "Foo"
+let lastName: String? = "Bar"
+
+let fullName = firstName + lastName
+
+// binary infix
+func + (
+  lhs: String?,
+  rhs: String?
+) -> String? {
+  switch(lhs, rhs) {
+  case(.none, .none):
+    return nil
+  case let(.some(value), .none),
+    let (.none, .some(value)):
+    return value
+  case let(.some(lhs), .some(rhs)):
+    return lhs + rhs
+  }
+}
+
+
+
+// unary prefix
+prefix operator ^
+prefix func ^ (value: String) -> String {
+  value.uppercased()
+}
+
+let lowerCaseName = "foo bar"
+let upperCaseName = ^lowerCaseName
+
+
+postfix operator *
+postfix func * (value: String) -> String {
+  "*** \(value) ***"
+}
+
+let withStarts = lowerCaseName*
+
+
+
+
+
+struct Person {
+  let name: String
+}
+
+struct Family {
+  let members: [Person]
+}
+
+func + (
+  lhs: Person,
+  rhs: Person
+) -> Family {
+  Family(
+    members: [
+      lhs,
+      rhs
+    ]
+  )
+}
+
+func + (
+  lhs: Family,
+  rhs: Person
+) -> Family {
+  var members = lhs.members
+  members.append(rhs)
+  return Family(members: members)
+}
+
+func + (
+  lhs: Family,
+  rhs: [Person]
+) -> Family {
+  var members = lhs.members
+  members.append(contentsOf: rhs)
+  return Family(members: members)
+}
+
+let mom = Person(name: "Mom")
+let dad = Person(name: "Dad")
+let son = Person(name: "Son")
+let daughter1 = Person(name: "Daughet 1")
+let daughter2 = Person(name: "Daughet 2")
+
+let family = mom + dad
+family.members.count
+let familyWithSon = family + son
+familyWithSon.members.count
+let familyWithDaughetrs = familyWithSon + [daughter1, daughter2]
+familyWithDaughetrs.members.count
