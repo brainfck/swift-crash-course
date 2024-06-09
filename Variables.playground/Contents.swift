5fe900c55d1999f85ac980bdc1364af0b68c4c51
brainfck <mykhailo.tsuber@gmail.com>
2024-06-09 23:17:05 +0200

    Variables

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -1,3 +1,65 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let myName = "Michael"
+let yourName = "Foo"
+
+var names = [
+  myName,
+  yourName
+]
+
+
+names.append("Bar")
+names.append("Baz")
+
+let foo = "Foo"
+var foo2 = foo
+
+// value types -> String is A Type of Structure
+
+foo2 = "Foo2"
+foo
+foo2
+
+let moreNames = [
+  "Foo",
+  "Bar"
+]
+
+var copy = moreNames
+copy.append("Baz")
+moreNames
+copy
+
+// NSMutableArray is a class so behavior is different
+
+let oldArray = NSMutableArray(
+  array: [
+    "Foo",
+    "Bar"
+  ]
+)
+
+oldArray.add("Baz")
+
+var newArray = oldArray
+newArray.add("Qux")
+oldArray
+newArray
+
+
+let someNames = NSMutableArray(
+  array: [
+    "Foo",
+    "Bar"
+  ]
+)
+
+func changeTheArray(_ array: NSArray) {
+  let array2 = array as! NSMutableArray
+  array2.add("Baz")
+}
+
+changeTheArray(someNames)
+someNames
+
